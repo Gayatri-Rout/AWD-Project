24670f7045fa5786764837168e33c0a8a49ab7b2
Gtree123 <70259060+Gtree123@users.noreply.github.com>
2022-04-16 15:56:12 +0530

    Update PopUpMenu.js

--- PopUpMenu.js
+++ PopUpMenu.js
@@ -1,25 +1,54 @@
 // Get the modal
-var modal = document.getElementById("myModal");
+
+var modalparent = document.getElementsByClassName("modal_multi");
 
 // Get the button that opens the modal
-var btn = document.getElementById("seeMenu");
+
+var modal_btn_multi = document.getElementsByClassName("myBtn_multi");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span_close_multi = document.getElementsByClassName("close_multi");
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+// When the user clicks the button, open the modal
+function setDataIndex() {
+
+    for (i = 0; i < modal_btn_multi.length; i++)
+    {
+        modal_btn_multi[i].setAttribute('data-index', i);
+        modalparent[i].setAttribute('data-index', i);
+        span_close_multi[i].setAttribute('data-index', i);
+    }
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+
+
+for (i = 0; i < modal_btn_multi.length; i++)
+{
+    modal_btn_multi[i].onclick = function() {
+        var ElementIndex = this.getAttribute('data-index');
+        modalparent[ElementIndex].style.display = "block";
+    };
+
+    // When the user clicks on <span> (x), close the modal
+    span_close_multi[i].onclick = function() {
+        var ElementIndex = this.getAttribute('data-index');
+        modalparent[ElementIndex].style.display = "none";
+    };
+
 }
 
-// When the user clicks anywhere outside of the modal, close it
+window.onload = function() {
+
+    setDataIndex();
+};
+
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+    if (event.target === modalparent[event.target.getAttribute('data-index')]) {
+        modalparent[event.target.getAttribute('data-index')].style.display = "none";
+    }
+
+    // OLD CODE
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+};
